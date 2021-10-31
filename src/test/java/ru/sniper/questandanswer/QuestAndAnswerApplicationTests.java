74902d61b6df63df85d08dca94287e6ffc1cdedb
sniper9284 <sniper-sochi@yandex.ru>
2021-10-31 12:31:50 +0300

    Initial commit

---
+++ src/test/java/ru/sniper/questandanswer/QuestAndAnswerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sniper.questandanswer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestAndAnswerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
